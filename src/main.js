668f18c556a7a756873dab439bf4270e083d152f
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2021-11-09 23:09:10 +0100

    Adicionado o package vuefinal-modal.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueNumber from "vue-number-animation";
+import { vfmPlugin } from "vue-final-modal";
 
 Vue.config.productionTip = false;
 Vue.use(VueNumber);
+
+Vue.use(
+  vfmPlugin({
+    key: "$vfm",
+    componentName: "VueFinalModal",
+    dynamicContainerName: "ModalsContainer",
+  })
+);
 
 new Vue({
   router,
