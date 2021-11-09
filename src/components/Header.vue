072f13dc27dae35ff31d6dbb800c3d30d23f60b2
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2021-11-09 09:15:23 +0100

    Aleração do Header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,192 @@
 <template>
-    <div>
-        
+  <header>
+    <div class="logo">
+      <a href="#">
+        <img src="../assets/logos/covid-19.png" alt="logo-covid" />
+      </a>
     </div>
+
+    <nav>
+      <div class="menu-icons open">
+        <i class="fa fa-bars"></i>
+      </div>
+
+      <div class="container-nav">
+        <ul class="links">
+          <li><a href="#start">Início</a></li>
+          <li><a href="#decrees">Decretos</a></li>
+          <li><a href="#commission">Comissão Multissectorial</a></li>
+          <li><a href="#emergency" class="emergency">Emergência</a></li>
+        </ul>
+        <div class="footer-nav" id="start">
+          <p>Copyright &copy; {{ anoActual }}</p>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      anoActual: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: var(--color-white);
+  width: 100%;
+  position: fixed;
+  z-index: 1900;
+  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.3),
+    0 1px 3px 1px rgba(60, 64, 67, 0.15);
+  height: 4rem;
+}
+
+.logo img {
+  height: 3rem;
+}
+
+.menu-icons {
+  cursor: pointer;
+  padding: 5px 12px 5px 12px;
+  font-size: 20pt;
+  border-radius: 7px;
+}
+
+.menu-icons.open {
+  margin-right: 10px;
+  background: var(--cor-amarela);
+  color: var(--cor-roxa);
+  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.3),
+    0 1px 3px 1px rgba(60, 64, 67, 0.15);
+}
+
+header .links {
+    width: 15rem;
+    height: 90vh;
+    background: var(--color-white);
+    position: fixed;
+    top: 48px;
+    left: -22rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: center;
+    z-index: 1500;
+    margin-left: -100px;
+    transition: all 650ms ease-in-out;
+    box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .3), 0 1px 3px 1px rgba(60, 64, 67, .15);
+}
+
+.links.open {
+    left: 3.5rem;
+}
+
+
+header .links li a {
+    color: var(--color-gray);
+    text-decoration: none;
+    transition: all 0.5s ease-in-out;
+}
+
+header .links li a:hover {
+    color: var(--color-rose);
+}
+
+header .links li {
+    list-style: none;
+    margin: 30px;
+}
+
+header .links .social li {
+    margin: 0px;
+}
+
+header .links .contacto {
+    padding: 9px 25px;
+    background-color: rgb(241, 196, 15);
+    color: var(--color-rose);
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    box-shadow: 1px 2px 5px var(--color-rose);
+    transition: all 650ms ease-in-out;
+}
+
+header .links .contacto:hover {
+    background: var(--cor-roxa);
+    color: var(--cor-branca);
+    box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .3), 0 1px 3px 1px rgba(60, 64, 67, .15);
+}
+
+header .links .footer-nav {
+    margin-left: -40px;
+}
+
+header .links .footer-nav p {
+    margin-left: 40px;
+    font-family: "Arial Rounded MT STD LIGTH";
+}
+
+@media screen and (min-width: 858px) {
+    h1 {
+        font-size: 25pt;
+        text-transform: uppercase;
+    }
+
+    /* Header */
+    header .links {
+        width: initial;
+        height: initial;
+        position: initial;
+        top: initial;
+        right: initial;
+        display: flex;
+        flex-direction: row;
+        transition: initial;
+        background: transparent;
+        margin-top: 10px;
+        box-shadow: none;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    header .menu-icons.open {
+        display: none;
+    }
+
+    header .links .menu {
+        display: flex;
+        flex-wrap: nowrap;
+    }
+
+    header .links li {
+        display: inline-block;
+        padding: 18px 15px;
+    }
+
+    header .links li {
+        list-style: none;
+        margin: initial;
+    }
+
+    header .footer-nav {
+        display: none;
+    }
+
+    /* Header */
+}
+</style>
