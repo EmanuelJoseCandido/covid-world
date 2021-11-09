89a3dcdf6d617915e1605393a54b1b0e2547c8e0
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2021-11-09 01:46:44 +0100

    Adicionado o componente Main.

---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <img src="../assets/gif/person-cold.gif" alt="person-cold" />
+    <h1>Site em construção</h1>
+  </main>
+</template>
+
+<script>
+//import http from "../services/http";
+
+export default {
+  name: "Main",
+
+  /* created() {
+    http
+      .get("countries/Angola?yesterday=true&strict=true&query")
+      .then((data) => {
+        console.log(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  }, */
+
+  methods: {},
+};
+</script>
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+main h1{
+    font-style: italic;
+}
+</style>
