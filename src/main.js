e8f51721e8abe2e4766b466d9fe127d08f8d8166
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2021-11-10 11:02:57 +0100

    Adicionado o package vue-awesome.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from "./router";
 import store from "./store";
 import VueNumber from "vue-number-animation";
 import VModal from "vue-js-modal";
+import Icon from "vue-awesome/components/Icon";
+import "./plugins/icons";
 
 Vue.config.productionTip = false;
+Vue.component("v-icon", Icon);
 Vue.use(VueNumber);
 Vue.use(VModal);
 
