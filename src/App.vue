1d1a483a02f8621fdea1f976de03d3532dc153fd
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2021-11-15 10:10:33 +0100

    Adicionada alguns packages.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
     <router-view />
+    <modals-container></modals-container>
+    <vue-scroll-progress-bar
+      height="2.5px"
+      backgroundColor="linear-gradient(to right, #fa705c, #ea4659)"
+    />
+    <back-to-top>
+      <button type="button" class="back-to-top">
+        <v-icon name="chevron-up" class="icon" />
+      </button>
+    </back-to-top>
   </div>
 </template>
+
+<script>
+import { ModalsContainer } from "vue-final-modal";
+export default {
+  components: {
+    ModalsContainer,
+  },
+};
+</script>
 
 <style>
 html,
@@ -18,5 +37,29 @@ body {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   color: #455a64;
+}
+
+.back-to-top {
+  background: var(--color-red);
+  color: var(--color-white);
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+/* ea4659 */
+
+@media screen and (max-width: 858px) {
+  .back-to-top {
+    background: var(--color-red);
+    color: var(--color-white);
+    width: 35px;
+    height: 35px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,24 @@ import store from "./store";
 import VueNumber from "vue-number-animation";
 import VModal from "vue-js-modal";
 import Icon from "vue-awesome/components/Icon";
+import VueYouTubeEmbed from "vue-youtube-embed";
+import VueScrollProgressBar from "@guillaumebriday/vue-scroll-progress-bar";
+import BackToTop from "vue-backtotop";
+import VueScrollTo from "vue-scrollto";
 import "./plugins/icons";
 
 Vue.config.productionTip = false;
 Vue.component("v-icon", Icon);
+Vue.use(VueScrollProgressBar);
+Vue.use(VueYouTubeEmbed);
+Vue.use(BackToTop);
 Vue.use(VueNumber);
 Vue.use(VModal);
+Vue.use(VueScrollTo, {
+  duration: 700,
+  easing: "ease",
+  offset: -65,
+});
 
 new Vue({
   router,
